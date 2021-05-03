036686e71ba8c9e946562442fa85ae4b172e70e9
Inna <kysarat01@gmail.com>
2021-05-04 01:09:13 +0300

    edit CourseworkApplication

--- coursework_2-master/coursework/src/main/java/com/example/coursework/CourseworkApplication.java
+++ coursework_2-master/coursework/src/main/java/com/example/coursework/CourseworkApplication.java
@@ -2,8 +2,10 @@ package com.example.coursework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, XADataSourceAutoConfiguration.class})
 public class CourseworkApplication {
 
 	public static void main(String[] args) {
