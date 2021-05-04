e62c25eaf4a2103907b0995a9b50ac52bbe3ebc3
Inna <kysarat01@gmail.com>
2021-05-05 01:48:29 +0300

    new version

--- coursework_2-master/coursework/src/main/java/com/example/coursework/reviews/Review.java
+++
@@ -1,9 +0,0 @@
-package com.example.coursework.reviews;
-
-import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-
